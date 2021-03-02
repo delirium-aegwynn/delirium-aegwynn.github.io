73d20814a9b0a90164426ab766bfbdfc098190a9
Suehnerhuppe <julian2110@gmx.de>
2021-03-02 17:12:45 +0100

    changes

--- Homepage/src/app/app.module.ts
+++ Homepage/src/app/app.module.ts
@@ -1,14 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ControlpanelComponent } from './controlpanel/controlpanel.component';
+import { EncounterComponent } from './encounter/encounter.component';
+import { PlayersComponent } from './players/players.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { NewsComponent } from './news/news.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule} from '@angular/material/tabs';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlpanelComponent,
+    EncounterComponent,
+    PlayersComponent,
+    TopnavComponent,
+    NewsComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatTabsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
