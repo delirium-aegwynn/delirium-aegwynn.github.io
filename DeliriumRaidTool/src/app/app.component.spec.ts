04bd24c0ebf1659b491079ee792465b3c28dbd49
Suehnerhuppe <julian2110@gmx.de>
2021-03-02 18:00:22 +0100

    revert

--- DeliriumRaidTool/src/app/app.component.spec.ts
+++ DeliriumRaidTool/src/app/app.component.spec.ts
@@ -1,14 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  });
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Homepage'`, () => {
+  it(`should have as title 'DeliriumRaidTool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Homepage');
+    expect(app.title).toEqual('DeliriumRaidTool');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Homepage app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('DeliriumRaidTool app is running!');
   });
 });
